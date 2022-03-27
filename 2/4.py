e824140eb7bfdfece9b06f8c28329f46834e19a6
InessaVos <voskanyan.inessa@gmail.com>
2022-03-28 02:57:38 +0300

    add com

---
+++ 2/4.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+my_str = "строка состоит из длинных и коротких слов"
+print(my_str.split( ))
+
+for ind, el in enumerate (["строка состоит из длинных и коротких слов"]), 1):
+    print (ind, el)
+#enumerate
