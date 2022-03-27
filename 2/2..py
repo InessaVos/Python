05736bc0e760e924b6b0459738d15ebfd8971ff1
InessaVos <voskanyan.inessa@gmail.com>
2022-03-28 01:16:07 +0300

    add com

--- 2/2..py
+++ 2/2..py
@@ -1,4 +1,7 @@
-list =
-el = int (input ("Введите любое число: "))
-list = (el)
-print list
+# -*- coding: utf-8 -*-
+result_list = []
+while len (result_list) <= 3 :
+    el = input("Введите элемет списка: ")
+    result_list.append(el)
+    print (result_list)
+print (result_list[1], result_list[0], result_list[3], result_list[2])
